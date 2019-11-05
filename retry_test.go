cb8ed4e747ce364891d58e4b00120c6b9c2cae3a
l.aminov <l.aminov@tinkoff.ru>
2019-11-05 21:43:44 +0500

    Add basic retry tests

--- retry_test.go
+++ retry_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"testing"
 	"time"
 )
@@ -37,4 +38,48 @@ func TestString(t *testing.T) {
 			t.Errorf("Must be %s, but got %s", test.res, test.stats.String())
 		}
 	}
+}
+
+func TestRetry(t *testing.T) {
+	tests := []*RetryConfig{
+		{
+			Maximum:  0,
+			Forever:  false,
+			Interval: 100 * time.Microsecond,
+		},
+		{
+			Increment:       true,
+			IntervalSeconds: 1,
+		},
+		{
+			Increment:       true,
+			Maximum:         10,
+			Interval:        100 * time.Microsecond,
+			IntervalMaximum: 50 * time.Microsecond,
+			Jitter:          true,
+		},
+	}
+	for _, test := range tests {
+		Retry(func(s *Stats) error {
+			return nil
+		}, test)
+	}
+	err := Retry(func(s *Stats) error {
+		return nil
+	}, &RetryConfig{
+		Interval: 0,
+		Forever:  true,
+	})
+	if err == nil {
+		t.Error("Must be an error, but got nil")
+	}
+	Retry(func(s *Stats) error {
+		if s.Attempt > 0 {
+			s.Break()
+		}
+		return errors.New("Error")
+	}, &RetryConfig{
+		Interval: 100 * time.Microsecond,
+		Forever:  true,
+	})
 }
